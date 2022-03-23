64d622ac02e6f95d4d731fc9a07c2ea0ed4af4c0
Stanley Lim <stannie@Stanleys-Air.lan>
2022-03-23 16:51:14 -0400

    made simple model and seeded

--- server/app.js
+++ server/app.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const path = require('path')
+const Sequelize = require('sequelize');
+const { STRING } = Sequelize.DataTypes;
+
+const db = new Sequelize('postgres://localhost/demo_db');
+
+const User = db.define('user', {
+  name: STRING,
+});
+
+const syncAndSeed = async () => {
+  try {
+    await db.sync({ force: true });
+    
+    await Promise.all(Array(50).fill().map((_, index) => User.create({ name: `user ${index}` })));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const app = express()
 
@@ -11,5 +29,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 }); 
 
-module.exports = app;
+module.exports = {
+  app,
+  syncAndSeed,
+};
 
